19bf2f35e9824701f263dbe4e9b644ce8d7bb5a7
Sarahinwn <riej98@gmail.com>
2021-04-12 12:52:38 -0500

    Enrutamiento de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,30 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
+import { RouterModule } from "@angular/router";
+import { VentasComponent } from './pages/ventas/ventas.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     FooterComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    VentasComponent,
+    InventarioComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'auth', component: AuthComponent },
+      { path: 'ventas', component: VentasComponent },
+      { path: 'inevntario', component: InventarioComponent },
+      { path: 'dashboard', component: DashboardComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
