9750290c042c0695da6519168b4f37fec125a52b
GibbyStack <carlosorozco4565@gmail.com>
2021-04-26 08:46:06 -0500

    Side bar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  menuItems: any = [
+    {
+      titulo: 'Administración',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        { titulo: 'Ventas', url: 'ventas' },
+        { titulo: 'Inventario', url: 'inventario' },
+        { titulo: 'Usuarios', url: 'usuarios' },
+      ],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
